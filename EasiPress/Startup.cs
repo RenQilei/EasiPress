a99d3f0d37d29c534b1c7c8b1aec70e6e00bc07d
Qilei Ren <renqilei@gmail.com>
2014-10-26 17:20:54 +0000

    Change the home page and some stylesheets.This is the first time uploading changes.

---
+++ EasiPress/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EasiPress.Startup))]
+namespace EasiPress
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
